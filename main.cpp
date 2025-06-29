1aef11b6b119591830a309856772057e52f03a0f
Trofim <dualdual657@gmail.com>
2025-06-29 19:14:30 +1000

    main commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+// otus-cpp-pro-hw01
+
 #include "lib.h"
 
 #include <iostream>
 
 int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
